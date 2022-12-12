6b774d7e6e40793ddaa63d90a7ed2d96fa2d283e
ElliotG <elliotgraebert@gmail.com>
2022-12-12 23:19:12 +0000

    Pick appropriate VM size

--- aks/main.tf
+++ aks/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_kubernetes_cluster" "coder" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "standard_d2ads_v5"
   }
 
   identity {
