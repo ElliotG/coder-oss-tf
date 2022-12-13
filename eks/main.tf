109ea457093a5f5c810e45eb63f1de0b094b376e
ElliotG <elliotgraebert@gmail.com>
2022-12-13 22:43:43 +0000

    Next iteration

--- eks/main.tf
+++ eks/main.tf
@@ -82,7 +82,7 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.coder.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.coder.certificate_authority[0].data)
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1alpha1"
     args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.coder.name]
     command     = "aws"
   }
