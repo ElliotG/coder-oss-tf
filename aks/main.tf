ba61d9da21b3a8020e9eb37f8b4f9e23cff3f0b2
ElliotG <elliotgraebert@gmail.com>
2022-12-13 00:23:05 -0700

    Added Azure AKS
    This PR adds AKS as an implementation option for installing CoderOSS within 10 minutes.

---
+++ aks/main.tf
@@ -0,0 +1,119 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+variable "coder_version" {
+    default = "0.12.7"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "coder" {
+  name     = "coder-resources"
+  location = "Central US"
+}
+
+resource "azurerm_kubernetes_cluster" "coder" {
+  name                = "coder-k8s-cluster"
+  location            = azurerm_resource_group.coder.location
+  resource_group_name = azurerm_resource_group.coder.name
+  dns_prefix          = "coder-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "standard_d2ads_v5"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+###############################################################
+# K8s configuration
+###############################################################
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.coder.kube_config.0.host
+  username               = azurerm_kubernetes_cluster.coder.kube_config.0.username
+  password               = azurerm_kubernetes_cluster.coder.kube_config.0.password
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_namespace" "coder_namespace" {
+  metadata {
+   name = "coder"
+  }
+}
+
+###############################################################
+# Coder configuration
+###############################################################
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.coder.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "pg_cluster" {
+  name       = "postgresql"
+  namespace  = kubernetes_namespace.coder_namespace.metadata.0.name
+  
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
+
+  set {
+    name  = "auth.username"
+    value = "coder"
+  }
+  
+  set {
+    name  = "auth.password"
+    value = "coder"
+  } 
+  
+  set {
+    name  = "auth.database"
+    value = "coder"
+  } 
+
+  set {
+    name  = "persistence.size"
+    value = "10Gi"
+  }  
+}
+
+resource "helm_release" "coder" {
+  name       = "coder"
+  namespace  = kubernetes_namespace.coder_namespace.metadata.0.name
+  
+  chart      = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
+
+  values = [
+    <<EOT
+coder:
+  env:
+    - name: CODER_PG_CONNECTION_URL
+      value: "postgres://coder:coder@postgresql.coder.svc.cluster.local:5432/coder?sslmode=disable"
+    - name: CODER_AUTO_IMPORT_TEMPLATES
+      value: "kubernetes"
+    EOT
+  ]
+
+  depends_on = [
+    helm_release.pg_cluster
+  ]  
+}
