887d67c30c4363f3416a538bd4f4548f1c0a1f19
ElliotG <elliotgraebert@gmail.com>
2022-12-13 03:33:58 +0000

    Fixed tf typo

--- aks/main.tf
+++ aks/main.tf
@@ -55,9 +55,9 @@ resource "azurerm_role_assignment" "coder" {
 provider "helm" {
   kubernetes {
     host                   = azurerm_kubernetes_cluster.coder.kube_config.0.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.coder.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.coder.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.coder.kube_config.0.cluster_ca_certificate)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.coder.kube_config.0.cluster_ca_certificate)
   }
 }
 
