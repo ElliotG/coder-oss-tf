a01d438382c238be0ec73289286c224a496c618c
ElliotG <elliotgraebert@gmail.com>
2022-12-15 21:35:03 +0000

    fixed typo

--- eks/main.tf
+++ eks/main.tf
@@ -88,7 +88,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  load_config_file.      = false
+  load_config_file       = false
 }
 
 resource "kubernetes_namespace" "coder_namespace" {
@@ -106,7 +106,7 @@ provider "helm" {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
-    load_config_file.      = false
+    load_config_file       = false
   }
 }
 
