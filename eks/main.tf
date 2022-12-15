fa866429382da1fb71f227be1e7ef1f1ea1f9846
ElliotG <elliotgraebert@gmail.com>
2022-12-15 22:05:06 +0000

    maybe a race condition?

--- eks/main.tf
+++ eks/main.tf
@@ -30,6 +30,7 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" : 1
