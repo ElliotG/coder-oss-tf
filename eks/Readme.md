c298f1029665c82a8f99cc69b15c91ce075d088f
ElliotG <elliotgraebert@gmail.com>
2022-12-16 19:03:45 +0000

    Trying out Spacelift again

--- eks/Readme.md
+++ eks/Readme.md
@@ -4,20 +4,25 @@
 2. Create an IAM User with the Administrator policy. Generate access keys and grant it console access. See bottom for notes.
 2. Fork this repo and set it up with [spacelift.io](https://spacelift.io/) or equivalent.
 3. Set [AWS_ACCESS_KEY_ID](https://registry.terraform.io/providers/hashicorp/aws/latest/docs) and [AWS_SECRET_ACCESS_KEY](https://registry.terraform.io/providers/hashicorp/aws/latest/docs)
-
-3. Set [GOOGLE_CREDENTIALS](https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#using-terraform-cloud) and TF_VAR_project with the project id from above
-4. Make sure to set the directory to gke
-4. Run and apply the Terraform (takes 10 minutes).
+4. Make sure to set the directory to eks
+4. Run and apply the Terraform (takes 15 minutes).
 
 ## Coder setup Instructions
 
-1. Navigate to the IP address of the load balancer (Google Cloud / Kubernetes Engine / Services & Ingress.
+1. Navigate to the DNS of the load balancer (AWS / EC2 / Load balancers).
 2. Create the initial username and password.
 3. Go to Templates / Kubernetes / Create Workspace and give the workspace a name.
 4. Within three minutes, the workspace should launch.
 5. Click the code-server button, and start coding.
 
-
-
 ## Why grant the Terraform user Console Access?
 Most of the kubernetes resources can only be managed if granted permissions via the kubernetes cluster role binding. The (AWS docs)[https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html] can step you through how to do this. It cannot be granted via IAM alone, except for the IAM user that originally created the EKS cluster. For this reason, it's easiest to grant the Terraform user console access so you can view the properties of the cluster. In production, you'd want to do this differently.
+
+## Why did I fork coder's helm chart?
+Due to a limitation in AWS, I needed to make a [change](https://github.com/coder/coder/pull/5448) to the Helm chart. Until this is fully released, I've forked the helm chart locally.
+
+## The VPC creation failed
+There's a rare AWS bug that can cause the VPC to fail to be created, but still create anyway. Sucks. Gotta delete and try again.
+
+## The Postgres pod is failing to get it's Persistent Volume created
+If you've tried to apply multiple times, there could be a duplicate volume. There's a wierd bug the way Terraform destroys the Helm chart that can leave the Persistent Volume around. Check to see if the volume already exists and manually delete it.
--- eks/main.tf
+++ eks/main.tf
@@ -17,39 +17,42 @@ provider "aws" {
 }
 
 ###############################################################
-# Set up the Networking Components
+# VPC configuration
 ###############################################################
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
   name = "coder"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   enable_dns_hostnames = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" : 1
-  }
+  cidr = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   private_subnet_tags = {
     "kubernetes.io/cluster/coder" : "shared"
   }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" : 1
+  }
 }
 
+###############################################################
+# EKS configuration
+###############################################################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_name    = "coder"
   cluster_version = "1.24"
-
   cluster_endpoint_public_access  = true
-
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -65,18 +68,16 @@ module "eks" {
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
   eks_managed_node_groups = {
     default = {
-      min_size     = 2
-      max_size     = 2
-      desired_size = 2
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.xlarge"]
       capacity_type  = "ON_DEMAND"
 
+      # Needed by the aws-ebs-csi-driver
       iam_role_additional_policies = {
         AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
       }
@@ -103,7 +104,6 @@ resource "kubernetes_namespace" "coder_namespace" {
   }
 }
 
-
 ###############################################################
 # Helm configuration
 ###############################################################
@@ -116,7 +116,7 @@ provider "helm" {
 }
 
 resource "helm_release" "pg_cluster" {
-  name       = "postgresql2"
+  name       = "postgresql"
   namespace  = kubernetes_namespace.coder_namespace.metadata.0.name
   
   repository = "https://charts.bitnami.com/bitnami"
@@ -147,6 +147,7 @@ resource "helm_release" "coder" {
   name       = "coder"
   namespace  = kubernetes_namespace.coder_namespace.metadata.0.name
   
+  # When v0.13.5 is released, we can unfork the repo
   # chart      = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
   chart        = "./helm"
 
@@ -155,23 +156,19 @@ resource "helm_release" "coder" {
 coder:
   env:
     - name: CODER_PG_CONNECTION_URL
-      value: "postgres://coder:coder@postgresql2.coder.svc.cluster.local:5432/coder?sslmode=disable"
+      value: "postgres://coder:coder@postgresql.coder.svc.cluster.local:5432/coder?sslmode=disable"
     - name: CODER_AUTO_IMPORT_TEMPLATES
       value: "kubernetes"
     EOT
   ]
 
   set {
-    name  = "service.sessionAffinity"
-    value = "None"
-  }
-  set {
     name  = "coder.service.sessionAffinity"
     value = "None"
   }
   set {
     name = "coder.image.tag"
-    value = "v0.12.7"
+    value = "v${var.coder_version}"
   }      
 
   depends_on = [
