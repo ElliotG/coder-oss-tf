ce6d61142967c04281a847c7b007183a46ff5ec4
Ben Potter <ben@coder.com>
2022-12-20 05:34:16 -0800

    add replica count & node pool config

--- gke/main.tf
+++ gke/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.3.0"
     }
   }
@@ -12,8 +12,27 @@ variable "region" {
   default = "us-central1"
 }
 variable "coder_version" {
-    default = "0.12.7"
+  default = "0.12.7"
 }
+# Multiple replicas is an enterprise feature:
+# https://coder.com/trial
+variable "coder_replicas" {
+  type    = number
+  default = 1
+}
+variable "enable_autopilot" {
+  type    = bool
+  default = true
+}
+# Set these if autopilot is disabled
+variable "machine_type" {
+  default = "e2-highmem-4"
+}
+variable "max_node_count" {
+  type    = number
+  default = 3
+}
+
 
 provider "google" {
   region  = var.region
@@ -36,13 +55,37 @@ resource "google_compute_subnetwork" "vpc_subnet" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "${var.project}-gke"
-  location = var.region
+  name       = "${var.project}-gke"
+  location   = var.region
   network    = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.vpc_subnet.name
-  enable_autopilot = true
+
   vertical_pod_autoscaling {
-    enabled = true 
+    enabled = true
+  }
+
+  # Allow GKE to manage nodes on your behalf
+  enable_autopilot = var.enable_autopilot
+  # Otherwise, set up a node pool
+  remove_default_node_pool = var.enable_autopilot ? null : true
+  initial_node_count       = var.enable_autopilot ? null : 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+
+  # Only create a seperate node pool if autopilot is disabled
+  count = var.enable_autopilot ? 0 : 1
+
+  name     = "coder-node-pool"
+  location = "us-central1"
+  cluster  = google_container_cluster.primary.name
+  autoscaling {
+    min_node_count = 1
+    max_node_count = var.max_node_count
+  }
+
+  node_config {
+    machine_type = var.machine_type
   }
 }
 
@@ -63,7 +106,7 @@ provider "kubernetes" {
 
 resource "kubernetes_namespace" "coder_namespace" {
   metadata {
-   name = "coder"
+    name = "coder"
   }
 }
 
@@ -82,9 +125,9 @@ provider "helm" {
 
 
 resource "helm_release" "pg_cluster" {
-  name       = "postgresql"
-  namespace  = kubernetes_namespace.coder_namespace.metadata.0.name
-  
+  name      = "postgresql"
+  namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "postgresql"
 
@@ -92,32 +135,33 @@ resource "helm_release" "pg_cluster" {
     name  = "auth.username"
     value = "coder"
   }
-  
+
   set {
     name  = "auth.password"
     value = "coder"
-  } 
-  
+  }
+
   set {
     name  = "auth.database"
     value = "coder"
-  } 
+  }
 
   set {
     name  = "persistence.size"
     value = "10Gi"
-  }  
+  }
 }
 
 resource "helm_release" "coder" {
-  name       = "coder"
-  namespace  = kubernetes_namespace.coder_namespace.metadata.0.name
-  
-  chart      = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
+  name      = "coder"
+  namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+
+  chart = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
 
   values = [
     <<EOT
 coder:
+  replicaCount: ${var.coder_replicas}
   env:
     - name: CODER_PG_CONNECTION_URL
       value: "postgres://coder:coder@postgresql.coder.svc.cluster.local:5432/coder?sslmode=disable"
@@ -125,8 +169,7 @@ coder:
       value: "kubernetes"
     EOT
   ]
-
   depends_on = [
     helm_release.pg_cluster
-  ]    
+  ]
 }
