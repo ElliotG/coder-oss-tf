796dcb7b8a5b380030291ca0ca6ffa407b0a7d34
ElliotG <elliotgraebert@gmail.com>
2022-12-27 02:32:45 +0000

    Trying valid do node size

--- digitalocean-k8s/main.tf
+++ digitalocean-k8s/main.tf
@@ -21,7 +21,7 @@ resource "digitalocean_kubernetes_cluster" "coder" {
 
   node_pool {
     name       = "default"
-    size       = "s-8vcpu-16gb"
+    size       = "s-2vcpu-4gb"
     node_count = 1
   }
 }
