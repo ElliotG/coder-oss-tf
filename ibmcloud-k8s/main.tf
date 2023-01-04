d40a1d72ebd03ce51609003e2f0af895f3e1c72b
ElliotG <elliotgraebert@gmail.com>
2023-01-04 21:16:47 +0000

    Initial exploration of IBMCloud

---
+++ ibmcloud-k8s/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
+  }
+}
+
+# Configure the IBM Provider
+provider "ibm" {
+  region = "us-east"
+}
+
+
+variable "coder_version" {
+  default = "0.13.6"
+}
+
+# resource "digitalocean_kubernetes_cluster" "coder" {
+#   name   = "coder"
+#   region = "nyc1"
+#   version = "1.25.4-do.0"
+
+#   node_pool {
+#     name       = "default"
+#     size       = "s-2vcpu-4gb"
+#     node_count = 1
+#   }
+# }
+
+###############################################################
+# K8s configuration
+###############################################################
+# provider "kubernetes" {
+#   host                   = digitalocean_kubernetes_cluster.coder.endpoint
+#   token                  = digitalocean_kubernetes_cluster.coder.kube_config[0].token
+#   cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.coder.kube_config[0].cluster_ca_certificate)
+# }
+
+# resource "kubernetes_namespace" "coder_namespace" {
+#   metadata {
+#     name = "coder"
+#   }
+# }
+
+###############################################################
+# Coder configuration
+###############################################################
+# provider "helm" {
+#   kubernetes {
+#     host                   = digitalocean_kubernetes_cluster.coder.endpoint
+#     token                  = digitalocean_kubernetes_cluster.coder.kube_config[0].token
+#     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.coder.kube_config[0].cluster_ca_certificate)
+#   }
+# }
+
+# resource "helm_release" "pg_cluster" {
+#   name      = "postgresql"
+#   namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "postgresql"
+
+#   set {
+#     name  = "auth.username"
+#     value = "coder"
+#   }
+
+#   set {
+#     name  = "auth.password"
+#     value = "coder"
+#   }
+
+#   set {
+#     name  = "auth.database"
+#     value = "coder"
+#   }
+
+#   set {
+#     name  = "persistence.size"
+#     value = "10Gi"
+#   }
+# }
+
+# resource "helm_release" "coder" {
+#   name      = "coder"
+#   namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+
+#   chart = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
+
+#   values = [
+#     <<EOT
+# coder:
+#   env:
+#     - name: CODER_PG_CONNECTION_URL
+#       value: "postgres://coder:coder@postgresql.coder.svc.cluster.local:5432/coder?sslmode=disable"
+#     - name: CODER_EXPERIMENTAL
+#       value: "true"
+#     EOT
+#   ]
+
+#   depends_on = [
+#     helm_release.pg_cluster
+#   ]
+# }
