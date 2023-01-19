9d86c8bc225c0c90982d6c5acf8c07b783b1ec3c
ElliotG <elliotgraebert@gmail.com>
2023-01-19 23:54:18 +0000

    will it blend

--- digitalocean-doks/main.tf
+++ digitalocean-doks/main.tf
@@ -51,60 +51,60 @@ resource "kubernetes_namespace" "coder_namespace" {
 ###############################################################
 # Coder configuration
 ###############################################################
-# provider "helm" {
-#   kubernetes {
-#     host                   = digitalocean_kubernetes_cluster.coder.endpoint
-#     token                  = digitalocean_kubernetes_cluster.coder.kube_config[0].token
-#     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.coder.kube_config[0].cluster_ca_certificate)
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = digitalocean_kubernetes_cluster.coder.endpoint
+    token                  = digitalocean_kubernetes_cluster.coder.kube_config[0].token
+    cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.coder.kube_config[0].cluster_ca_certificate)
+  }
+}
 
-# resource "helm_release" "pg_cluster" {
-#   name      = "postgresql"
-#   namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+resource "helm_release" "pg_cluster" {
+  name      = "postgresql"
+  namespace = kubernetes_namespace.coder_namespace.metadata.0.name
 
-#   repository = "https://charts.bitnami.com/bitnami"
-#   chart      = "postgresql"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
 
-#   set {
-#     name  = "auth.username"
-#     value = "coder"
-#   }
+  set {
+    name  = "auth.username"
+    value = "coder"
+  }
 
-#   set {
-#     name  = "auth.password"
-#     value = "${var.db_password}"
-#   }
+  set {
+    name  = "auth.password"
+    value = "${var.db_password}"
+  }
 
-#   set {
-#     name  = "auth.database"
-#     value = "coder"
-#   }
+  set {
+    name  = "auth.database"
+    value = "coder"
+  }
 
-#   set {
-#     name  = "persistence.size"
-#     value = "10Gi"
-#   }
-# }
+  set {
+    name  = "persistence.size"
+    value = "10Gi"
+  }
+}
 
-# resource "helm_release" "coder" {
-#   name      = "coder"
-#   namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+resource "helm_release" "coder" {
+  name      = "coder"
+  namespace = kubernetes_namespace.coder_namespace.metadata.0.name
 
-#   chart = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
+  chart = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
 
-#   values = [
-#     <<EOT
-# coder:
-#   env:
-#     - name: CODER_PG_CONNECTION_URL
-#       value: "postgres://coder:${var.db_password}@${helm_release.pg_cluster.name}.coder.svc.cluster.local:5432/coder?sslmode=disable"
-#     - name: CODER_EXPERIMENTAL
-#       value: "true"
-#     EOT
-#   ]
+  values = [
+    <<EOT
+coder:
+  env:
+    - name: CODER_PG_CONNECTION_URL
+      value: "postgres://coder:${var.db_password}@${helm_release.pg_cluster.name}.coder.svc.cluster.local:5432/coder?sslmode=disable"
+    - name: CODER_EXPERIMENTAL
+      value: "true"
+    EOT
+  ]
 
-#   depends_on = [
-#     helm_release.pg_cluster
-#   ]
-# }
+  depends_on = [
+    helm_release.pg_cluster
+  ]
+}
