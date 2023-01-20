7c8d5f7e0cbc3492515741debdb28a7e582438fd
ElliotG <elliotgraebert@gmail.com>
2023-01-20 16:50:35 +0000

    here goes nothin

--- linode-lke/main.tf
+++ linode-lke/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source = "linode/linode"
     }
   }
 }
@@ -19,33 +19,24 @@ variable "db_password" {
 ###############################################################
 # K8s configuration
 ###############################################################
-# See: https://registry.terraform.io/providers/scaleway/scaleway/latest/docs#authentication
-# Set SCW_ACCESS_KEY, SCW_SECRET_KEY, and SCW_DEFAULT_PROJECT_ID
-provider "scaleway" {}
+# Set LINODE_TOKEN 
+provider "linode" {}
 
-resource "scaleway_k8s_cluster" "coder" {
-  name    = "coder"
-  version = "1.24.3"
-  cni     = "cilium"
+resource "linode_lke_cluster" "coder" {
+    label       = "coder"
+    k8s_version = "1.24"
+    region      = "us-central"
+    tags        = ["prod"]
+
+    pool {
+        type  = "g6-standard-1"
+        count = 1
+    }
 }
 
-resource "scaleway_k8s_pool" "coder" {
-  cluster_id = scaleway_k8s_cluster.coder.id
-  name       = "coder"
-  node_type  = "DEV1-M"
-  size       = 1
-}
-
-# This was copied directly from the Scaleway Terraform docs I don't believe this protects 
-# from the weird provider-depending-on-resources issue. However, since it's directly from
-# their docs, I'll keep it this way. See Digital Ocean for a simpler approach.
-resource "null_resource" "kubeconfig" {
-  depends_on = [scaleway_k8s_pool.coder] # at least one pool here
-  triggers = {
-    host                   = scaleway_k8s_cluster.coder.kubeconfig[0].host
-    token                  = scaleway_k8s_cluster.coder.kubeconfig[0].token
-    cluster_ca_certificate = scaleway_k8s_cluster.coder.kubeconfig[0].cluster_ca_certificate
-  }
+//Export this cluster's attributes
+output "kubeconfig" {
+   value = linode_lke_cluster.coder.kubeconfig
 }
 
 provider "kubernetes" {
@@ -60,63 +51,63 @@ resource "kubernetes_namespace" "coder_namespace" {
   }
 }
 
-# ###############################################################
-# # Coder configuration
-# ###############################################################
-provider "helm" {
-  kubernetes {
-    host                   = null_resource.kubeconfig.triggers.host
-    token                  = null_resource.kubeconfig.triggers.token
-    cluster_ca_certificate = base64decode(null_resource.kubeconfig.triggers.cluster_ca_certificate)
-  }
-}
+# # ###############################################################
+# # # Coder configuration
+# # ###############################################################
+# provider "helm" {
+#   kubernetes {
+#     host                   = null_resource.kubeconfig.triggers.host
+#     token                  = null_resource.kubeconfig.triggers.token
+#     cluster_ca_certificate = base64decode(null_resource.kubeconfig.triggers.cluster_ca_certificate)
+#   }
+# }
 
-resource "helm_release" "pg_cluster" {
-  name      = "postgresql"
-  namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+# resource "helm_release" "pg_cluster" {
+#   name      = "postgresql"
+#   namespace = kubernetes_namespace.coder_namespace.metadata.0.name
 
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "postgresql"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "postgresql"
 
-  set {
-    name  = "auth.username"
-    value = "coder"
-  }
+#   set {
+#     name  = "auth.username"
+#     value = "coder"
+#   }
 
-  set {
-    name  = "auth.password"
-    value = var.db_password
-  }
+#   set {
+#     name  = "auth.password"
+#     value = var.db_password
+#   }
 
-  set {
-    name  = "auth.database"
-    value = "coder"
-  }
+#   set {
+#     name  = "auth.database"
+#     value = "coder"
+#   }
 
-  set {
-    name  = "persistence.size"
-    value = "10Gi"
-  }
-}
+#   set {
+#     name  = "persistence.size"
+#     value = "10Gi"
+#   }
+# }
 
-resource "helm_release" "coder" {
-  name      = "coder"
-  namespace = kubernetes_namespace.coder_namespace.metadata.0.name
+# resource "helm_release" "coder" {
+#   name      = "coder"
+#   namespace = kubernetes_namespace.coder_namespace.metadata.0.name
 
-  chart = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
+#   chart = "https://github.com/coder/coder/releases/download/v${var.coder_version}/coder_helm_${var.coder_version}.tgz"
 
-  values = [
-    <<EOT
-coder:
-  env:
-    - name: CODER_PG_CONNECTION_URL
-      value: "postgres://coder:${var.db_password}@${helm_release.pg_cluster.name}.coder.svc.cluster.local:5432/coder?sslmode=disable"
-    - name: CODER_EXPERIMENTAL
-      value: "true"
-    EOT
-  ]
+#   values = [
+#     <<EOT
+# coder:
+#   env:
+#     - name: CODER_PG_CONNECTION_URL
+#       value: "postgres://coder:${var.db_password}@${helm_release.pg_cluster.name}.coder.svc.cluster.local:5432/coder?sslmode=disable"
+#     - name: CODER_EXPERIMENTAL
+#       value: "true"
+#     EOT
+#   ]
 
-  depends_on = [
-    helm_release.pg_cluster
-  ]
-}
+#   depends_on = [
+#     helm_release.pg_cluster
+#   ]
+# }
