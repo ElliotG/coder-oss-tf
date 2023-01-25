1d2cb4d16f328518f282cda7f30112c83626eb51
ElliotG <elliotgraebert@gmail.com>
2023-01-25 13:03:07 -0700

    Update main.tf

--- azure-aks/main.tf
+++ azure-aks/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_kubernetes_cluster" "coder" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "standard_d2ads_v5"
+    vm_size    = "standard_d2s_v3"
   }
 
   identity {
